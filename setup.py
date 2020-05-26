d6fbe368be9c8a9ab1cc033e19b8db0c7e8ffae0
Thomas Röhl <Thomas.Roehl@googlemail.com>
2020-05-26 17:04:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,67 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+import re
+
+long_description='''Gathering the system state before benchmarking and other tests on compute
+systems. MachineState reads common sysfs and procfs files as well as executes commands to get all
+required data. For reproducibility, the once saved system state can be compared to the current state
+before re-running a test.
+'''
+
+def find_version():
+    with open("machinestate.py") as msfp:
+        lines = msfp.read().split("\n")
+        for line in lines:
+            mat = re.search(r"^MACHINESTATE_VERSION = ['\"]([^'\"]*)['\"]", line)
+            if mat:
+                return mat.group(1)
 
 setup(name='MachineState',
-    version='0.1',
+    version=find_version(),
+
     description='Gather machine state and dump it as JSON document',
-    long_description='',
+    long_description=long_description,
+
+    # Author details
     author='Thomas Gruber',
     author_email='thomas.gruber@fau.de',
+
+    # The project's main homepage.
     url='https://github.com/RRZE-HPC/Artifact-description',
+
+    # License
     license='GPLv3',
     py_modules=['machinestate'],
     entry_points = {
         'console_scripts': ['machinestate=machinestate:main'],
     },
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3',
-        'Environment :: Console',
         'Development Status :: 3 - Alpha',
+
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
+
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Topic :: Education :: Testing',
-        'Topic :: System',
+
         'Topic :: System :: Benchmark',
         'Topic :: System :: Hardware',
-        'Topic :: System :: Hardware :: Symmetric Multi-processing',
         'Topic :: System :: Operating System',
         'Topic :: System :: Systems Administration',
-        ],
-    keywords='benchmarking, linux, system state'
+        'Topic :: Education :: Testing',
+
+        'Operating System :: POSIX :: Linux',
+        'Environment :: Console',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    keywords='benchmarking, linux, system state, topology'
 )

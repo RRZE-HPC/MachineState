48868efbbcbc0d58f1768c16a9b41465309ee7ed
Thomas Roehl <thomas.roehl@fau.de>
2020-05-30 02:29:31 +0200

    Functional tests for PathMatchInfoGroup

--- tests/test_pathmatchinfo.py
+++ tests/test_pathmatchinfo.py
@@ -2,19 +2,23 @@
 """
 High-level tests for the class PathMatchInfoGroup
 """
-import os
-import sys
+import os, os.path
+import sys, glob
 import unittest
 import tempfile
 import shutil
 import stat
-from machinestate import PathMatchInfoGroup
+from machinestate import PathMatchInfoGroup, InfoGroup
 from locale import getpreferredencoding
 
 ENCODING = getpreferredencoding()
 
-class TestClass:
-    pass
+class TestClass(InfoGroup):
+    def __init__(self, ident, extended=False, anon=False, searchpath=""):
+        super(TestClass, self).__init__(anon=anon, extended=extended, name="File{}".format(ident))
+        path = os.path.join(searchpath, "{}*".format(ident))
+        files = glob.glob(path)
+        self.files["File{}".format(ident)] = (files[0], r"(.+)")
 
 
 class TestPathMatchInfoGroupBase(unittest.TestCase):
@@ -46,7 +50,7 @@ class TestPathMatchInfoGroupBase(unittest.TestCase):
         self.assertEqual(cls.searchpath, None)
     def test_searchpathExist(self):
         cls = PathMatchInfoGroup(searchpath="/tmp/*")
-        self.assertEqual(cls.searchpath, None)
+        self.assertEqual(cls.searchpath, "/tmp/*")
     def test_searchpathNone(self):
         cls = PathMatchInfoGroup(searchpath=None)
         self.assertEqual(cls.searchpath, None)
@@ -77,3 +81,117 @@ class TestPathMatchInfoGroupBase(unittest.TestCase):
     def test_subargsList(self):
         cls = PathMatchInfoGroup(subargs=[])
         self.assertEqual(cls.subargs, {})
+
+class TestInfoGroupFunction(unittest.TestCase):
+    def setUp(self):
+        # Create a temporary directory
+        self.temp_dir = tempfile.mkdtemp()
+        self.temp_files = {"File{}".format(x) : tempfile.mkstemp(prefix=str(x), dir=self.temp_dir) for x in range(4)}
+        for tkey in self.temp_files:
+            tfp, tfname = self.temp_files[tkey]
+            os.pwrite(tfp, bytes("{}\n".format(tkey), ENCODING), 0)
+            os.close(tfp)
+
+    def tearDown(self):
+        # Remove the directory after the test
+        shutil.rmtree(self.temp_dir)
+
+    def test_validCreate(self):
+        searchpath = os.path.join(self.temp_dir, "*")
+        cls = PathMatchInfoGroup(searchpath=searchpath, match=r".*/(\d).*", subclass=TestClass, subargs={"searchpath": self.temp_dir})
+        self.assertEqual(cls._instances, [])
+    def test_validGenerate(self):
+        searchpath = os.path.join(self.temp_dir, "*")
+        cls = PathMatchInfoGroup(searchpath=searchpath, match=r".*/(\d).*", subclass=TestClass, subargs={"searchpath": self.temp_dir})
+        cls.generate()
+        self.assertNotEqual(cls._instances, [])
+        self.assertEqual(len(cls._instances), 4)
+        self.assertEqual(cls._data, {})
+        for inst in cls._instances:
+            self.assertEqual(inst._instances, [])
+            self.assertEqual(inst._data, {})
+    def test_validUpdate(self):
+        searchpath = os.path.join(self.temp_dir, "*")
+        cls = PathMatchInfoGroup(searchpath=searchpath, match=r".*/(\d).*", subclass=TestClass, subargs={"searchpath": self.temp_dir})
+        cls.generate()
+        cls.update()
+        self.assertEqual(cls._data, {})
+        for inst in cls._instances:
+            self.assertEqual(inst._instances, [])
+            self.assertNotEqual(inst._data, {})
+    def test_validGet(self):
+        searchpath = os.path.join(self.temp_dir, "*")
+        cls = PathMatchInfoGroup(searchpath=searchpath, match=r".*/(\d).*", subclass=TestClass, subargs={"searchpath": self.temp_dir})
+        cls.generate()
+        cls.update()
+        outdict = cls.get()
+        self.assertEqual(cls._data, {})
+        for inst in cls._instances:
+            self.assertEqual(inst._instances, [])
+            self.assertNotEqual(inst._data, {})
+        for i, key in enumerate(self.temp_files.keys()):
+            for subkey in outdict[key]:
+                self.assertEqual(key, subkey)
+                self.assertEqual(key, outdict[key][subkey])
+    def test_invalidCreate(self):
+        searchpath = os.path.join(self.temp_dir, "*abc")
+        cls = PathMatchInfoGroup(searchpath=searchpath, match=r".*/(\d).*", subclass=TestClass, subargs={"searchpath": self.temp_dir})
+        self.assertEqual(cls._instances, [])
+        self.assertEqual(cls._data, {})
+        self.assertEqual(cls.searchpath, searchpath)
+    def test_invalidGenerate(self):
+        searchpath = os.path.join(self.temp_dir, "*abc")
+        cls = PathMatchInfoGroup(searchpath=searchpath, match=r".*/(\d).*", subclass=TestClass, subargs={"searchpath": self.temp_dir})
+        cls.generate()
+        self.assertEqual(cls._instances, [])
+        self.assertEqual(cls._data, {})
+        self.assertEqual(cls.searchpath, searchpath)
+    def test_invalidUpdate(self):
+        searchpath = os.path.join(self.temp_dir, "*abc")
+        cls = PathMatchInfoGroup(searchpath=searchpath, match=r".*/(\d).*", subclass=TestClass, subargs={"searchpath": self.temp_dir})
+        cls.generate()
+        cls.update()
+        self.assertEqual(cls._instances, [])
+        self.assertEqual(cls._data, {})
+        self.assertEqual(cls.searchpath, searchpath)
+    def test_invalidGet(self):
+        searchpath = os.path.join(self.temp_dir, "*abc")
+        cls = PathMatchInfoGroup(searchpath=searchpath, match=r".*/(\d).*", subclass=TestClass, subargs={"searchpath": self.temp_dir})
+        cls.generate()
+        cls.update()
+        outdict = cls.get()
+        self.assertEqual(outdict, {})
+        self.assertEqual(cls._instances, [])
+        self.assertEqual(cls._data, {})
+        self.assertEqual(cls.searchpath, searchpath)
+    def test_validCreateInvalidClass(self):
+        searchpath = os.path.join(self.temp_dir, "*")
+        cls = PathMatchInfoGroup(searchpath=searchpath, match=r".*/(\d).*", subclass=unittest.TestCase, subargs={"searchpath": self.temp_dir})
+        self.assertEqual(cls._instances, [])
+        self.assertEqual(cls.subclass, unittest.TestCase)
+    def test_validGenerateInvalidClass(self):
+        searchpath = os.path.join(self.temp_dir, "*")
+        cls = PathMatchInfoGroup(searchpath=searchpath, match=r".*/(\d).*", subclass=unittest.TestCase, subargs={"searchpath": self.temp_dir})
+        self.assertRaises(TypeError, cls.generate)
+    def test_validGenerateInvalidClass(self):
+        searchpath = os.path.join(self.temp_dir, "*")
+        cls = PathMatchInfoGroup(searchpath=searchpath, match=r".*/(\d).*", subclass=unittest.TestCase, subargs={"searchpath": self.temp_dir})
+        try:
+            cls.generate()
+        except:
+            pass
+        cls.update()
+        self.assertEqual(cls._instances, [])  
+        self.assertEqual(cls._data, {})
+    def test_validGenerateInvalidClass(self):
+        searchpath = os.path.join(self.temp_dir, "*")
+        cls = PathMatchInfoGroup(searchpath=searchpath, match=r".*/(\d).*", subclass=unittest.TestCase, subargs={"searchpath": self.temp_dir})
+        try:
+            cls.generate()
+        except:
+            pass
+        cls.update()
+        outdict = cls.get()
+        self.assertEqual(cls._instances, [])  
+        self.assertEqual(cls._data, {})
+        self.assertEqual(outdict, {})

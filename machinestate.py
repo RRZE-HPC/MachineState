7a4883006d9165cc8285fb5dcd3e55f2e0551018
Thomas Roehl <thomas.roehl@fau.de>
2020-06-19 14:57:08 +0200

    Add class for InterruptAffinty

--- machinestate.py
+++ machinestate.py
@@ -958,6 +958,7 @@ class MachineState(MultiClassInfoGroup):
             UsersInfo,
             CpuAffinity,
             ModulesInfo,
+            IrqAffinity,
         ]
         self.classargs = [{} for x in self.classlist]
 
@@ -2250,6 +2251,30 @@ class ModulesInfo(InfoGroup):
     def parsemodules(value):
         slist = re.split("\n", value)
         return slist[1:]
+
+################################################################################
+# Infos about interrupt handling
+# see https://pyperf.readthedocs.io/en/latest/system.html#system-cmd-ops
+################################################################################
+class IrqAffinityClass(InfoGroup):
+    '''Class to read information about one interrupt affinity'''
+    def __init__(self, irq, extended=False, anonymous=False):
+        super(IrqAffinityClass, self).__init__(name="irq{}".format(irq),
+                                               extended=extended,
+                                               anonymous=anonymous)
+        self.addf("SMPAffinity", "/proc/irq/{}/smp_affinity".format(irq), parse=masktolist)
+
+class IrqAffinity(PathMatchInfoGroup):
+    '''Class to read information about one interrupt affinity'''
+    def __init__(self, extended=False, anonymous=False):
+        super(IrqAffinity, self).__init__(name="IrqAffinity",
+                                          extended=extended,
+                                          anonymous=anonymous,
+                                          searchpath="/proc/irq/*",
+                                          match=".*/(\d+)",
+                                          subclass=IrqAffinityClass)
+        self.addf("DefaultSMPAffinity", "/proc/irq/default_smp_affinity", parse=masktolist)
+
 
 ################################################################################
 # Infos about InfiniBand adapters

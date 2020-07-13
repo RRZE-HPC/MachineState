deaeaeabb319165ce8d775d0132c26d5ca456748
Thomas Roehl <thomas.roehl@fau.de>
2020-07-13 12:29:41 +0200

    Check return value when getting the cgroup base path. See #3.

--- machinestate.py
+++ machinestate.py
@@ -1483,16 +1483,17 @@ class CgroupInfo(InfoGroup):
         super(CgroupInfo, self).__init__(name="Cgroups", extended=extended, anonymous=anonymous)
         csetmat = re.compile(r"\d+\:cpuset\:([/\w\d\-\._]*)")
         cset = process_file(("/proc/self/cgroup", csetmat))
-        base = pjoin("/sys/fs/cgroup/cpuset", cset.strip("/"))
-        self.addf("CPUs", pjoin(base, "cpuset.cpus"), r"(.+)", tointlist)
-        self.addf("Mems", pjoin(base, "cpuset.mems"), r"(.+)", tointlist)
-        self.required("CPUs", "Mems")
-        if extended:
-            names = ["CPUs.effective", "Mems.effective"]
-            files = ["cpuset.effective_cpus", "cpuset.effective_mems"]
-            for key, fname in zip(names, files):
-                self.addf(key, pjoin(base, fname), r"(.+)", tointlist)
-                self.required(key)
+        if cset is not None:
+            base = pjoin("/sys/fs/cgroup/cpuset", cset.strip("/"))
+            self.addf("CPUs", pjoin(base, "cpuset.cpus"), r"(.+)", tointlist)
+            self.addf("Mems", pjoin(base, "cpuset.mems"), r"(.+)", tointlist)
+            self.required("CPUs", "Mems")
+            if extended:
+                names = ["CPUs.effective", "Mems.effective"]
+                files = ["cpuset.effective_cpus", "cpuset.effective_mems"]
+                for key, fname in zip(names, files):
+                    self.addf(key, pjoin(base, fname), r"(.+)", tointlist)
+                    self.required(key)
 
 ################################################################################
 # Infos about the writeback workqueue

b86f241da8e8f5f2e84ec814dc456b57301891f7
Thomas Roehl <thomas.roehl@fau.de>
2020-07-17 12:25:37 +0200

    Increase version

--- machinestate.py
+++ machinestate.py
@@ -138,7 +138,7 @@ NVIDIA_PATH = "/opt/nvidia/bin"
 ################################################################################
 # Version information
 ################################################################################
-MACHINESTATE_VERSION = "0.2.1-dev1"
+MACHINESTATE_VERSION = "0.2"
 MACHINESTATE_SCHEMA_VERSION = "v1"
 __version__ = MACHINESTATE_VERSION
 

657fe56fef3f863a6cc02f7edd3083d701b76d75
Thomas Roehl <thomas.roehl@fau.de>
2020-07-22 03:20:32 +0200

    Fix for NumaInfo on macOS

--- machinestate.py
+++ machinestate.py
@@ -1359,13 +1359,13 @@ class CpuFrequency(PathMatchInfoGroup):
 ################################################################################
 # NUMA Topology
 ################################################################################
-class NumaInfoMacOS(InfoGroup):
-    def __init__(self, anonymous=False, extended=False):
-        super(NumaInfoMacOS, self).__init__(anonymous=anonymous, extended=extended)
-        self.name = "NumaNode0"
+class NumaInfoMacOSClass(InfoGroup):
+    def __init__(self, node, anonymous=False, extended=False):
+        super(NumaInfoMacOSClass, self).__init__(anonymous=anonymous, extended=extended)
+        self.name = "NumaNode{}".format(node)
         self.addc("MemTotal", "sysctl", "-a", r"hw.memsize: (\d+)", int)
         self.addc("MemFree", "sysctl", "-a", r"vm.page_free_count: (\d+)", MemInfoMacOS.pagescale)
-        self.addc("CpuList", "sysctl", "-a", r"hw.cacheconfig: (\d+)", NumaInfoMacOS.cpulist)
+        self.addc("CpuList", "sysctl", "-a", r"hw.cacheconfig: (\d+)", NumaInfoMacOSClass.cpulist)
     @staticmethod
     def cpulist(value):
         ncpu = process_cmd(("sysctl", "-n hw.ncpu", r"(\d+)", int))
@@ -1375,6 +1375,13 @@ class NumaInfoMacOS(InfoGroup):
                 clist.append(list(range(i*ncpu, (i+1)*ncpu)))
         return clist
 
+class NumaInfoMacOS(ListInfoGroup):
+    def __init__(self, anonymous=False, extended=False):
+        super(NumaInfoMacOS, self).__init__(name="NumaInfo", anonymous=anonymous, extended=extended)
+        self.subclass = NumaInfoMacOSClass
+        num_packs = process_cmd(("sysctl", "-n hw.packages", r"(\d+)", int))
+        if num_packs is not None and num_packs > 0:
+            self.userlist = list(range(num_packs))
 
 class NumaInfoHugepagesClass(InfoGroup):
     def __init__(self, size, extended=False, anonymous=False, node=0):

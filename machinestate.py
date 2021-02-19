b3534c8fb8a29a755b280299c9b16fdb6db11f9e
Thomas Roehl <Thomas.Roehl@googlemail.com>
2021-02-19 18:40:48 +0100

    Add information about khugepaged to TransparenHugepages class

--- machinestate.py
+++ machinestate.py
@@ -1766,6 +1766,18 @@ class WritebackInfo(InfoGroup):
 ################################################################################
 # Infos about transparent hugepages
 ################################################################################
+class TransparentHugepagesDaemon(InfoGroup):
+    def __init__(self, extended=False, anonymous=False):
+        super(TransparentHugepagesDaemon, self).__init__(name="TransparentHugepagesDaemon",
+                                                         extended=extended,
+                                                         anonymous=anonymous)
+        base = "/sys/kernel/mm/transparent_hugepage/khugepaged"
+        self.addf("Defrag", pjoin(base, "defrag"), r"(\d+)", int)
+        self.addf("PagesToScan", pjoin(base, "pages_to_scan"), r"(\d+)", int)
+        self.addf("ScanSleepMillisecs", pjoin(base, "scan_sleep_millisecs"), r"(\d+)", int)
+        self.addf("AllocSleepMillisecs", pjoin(base, "alloc_sleep_millisecs"), r"(\d+)", int)
+        self.required(["Defrag", "PagesToScan", "ScanSleepMillisecs", "AllocSleepMillisecs"])
+
 class TransparentHugepages(InfoGroup):
     def __init__(self, extended=False, anonymous=False):
         super(TransparentHugepages, self).__init__(name="TransparentHugepages",
@@ -1773,8 +1785,11 @@ class TransparentHugepages(InfoGroup):
                                                    anonymous=anonymous)
         base = "/sys/kernel/mm/transparent_hugepage"
         self.addf("State", pjoin(base, "enabled"), r".*\[(.*)\].*")
+        self.addf("Defrag", pjoin(base, "defrag"), r".*\[(.*)\].*")
+        self.addf("ShmemEnabled", pjoin(base, "shmem_enabled"), r".*\[(.*)\].*")
         self.addf("UseZeroPage", pjoin(base, "use_zero_page"), r"(\d+)", tobool)
-        self.required(["State", "UseZeroPage"])
+        self.required(["State", "UseZeroPage", "Defrag", "ShmemEnabled"])
+        self._instances = [TransparentHugepagesDaemon(extended, anonymous)]
 
 
 ################################################################################

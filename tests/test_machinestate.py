2c85cc0661b7953c5ed81a9d5a83c1635f889b5c
Thomas Roehl <Thomas.Roehl@googlemail.com>
2021-03-07 14:30:14 +0100

    Update tests with from_dict tests

--- tests/test_machinestate.py
+++ tests/test_machinestate.py
@@ -17,12 +17,25 @@ ENCODING = getpreferredencoding()
 class TestMachineState(unittest.TestCase):
     def test_getJson(self):
         cls = MachineState()
-        outstr = cls.get_json()
+        outstr = cls.get_json(meta=False)
         self.assertEqual(outstr, "{}")
+    def test_getJsonMeta(self):
+        cls = MachineState()
+        outstr = cls.get_json(meta=True)
+        self.assertEqual(outstr, "{\n    \"_meta\": \"MachineState()\"\n}")
     def test_CompareJson(self):
         cls = MachineState()
         cls.generate()
         cls.update()
         outstr = cls.get_json()
         self.assertNotEqual(outstr, "{}")
-        self.assertTrue(cls == outstr)
+        self.assertTrue(cls.get_json() == outstr)
+    def test_fromDictCompare(self):
+        ms = MachineState();
+        ms.generate();
+        ms.update()
+        msdict = ms.get(meta=True)
+        mscopy = MachineState.from_dict(msdict)
+        self.assertEqual(ms, mscopy)
+        self.assertEqual(ms.get(meta=False), mscopy.get(meta=False))
+        self.assertEqual(ms.get(meta=True), mscopy.get(meta=True))

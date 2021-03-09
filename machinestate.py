66cbb5abec2e2fa33c7768113e62d4d243295f36
Thomas Roehl <Thomas.Roehl@googlemail.com>
2021-03-09 01:15:55 +0100

    Add some tags to html output to make it valid HTML5. Some issues left

--- machinestate.py
+++ machinestate.py
@@ -2989,9 +2989,12 @@ base_css = """
 """
 
 base_html = """
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta name "viewport" content="width=device-width, initial-scale=1">
+<title>MachineState</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="UTF-8">
 {css}
 </head>
 
